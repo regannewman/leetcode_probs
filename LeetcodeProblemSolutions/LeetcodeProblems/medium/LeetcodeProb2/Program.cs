30bf7ce820bca598a13c0a26d22e70f6fb7a1a7c
Regan Newman <renewman315@gmail.com>
2024-08-16 17:07:06 -0400

    hi

---
+++ LeetcodeProblemSolutions/LeetcodeProblems/medium/LeetcodeProb2/Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+
+using System;
+namespace LeetcodeProb2{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IntToRoman(3943));
+          
+        }
+
+        public static string IntToRoman(int num){
+            string toReturn = "";
+            Dictionary<int, char> numerals = new Dictionary<int, char>{
+
+                { 1, 'I'},
+                { 5, 'V'},
+                { 10, 'X'},
+                { 50, 'L'},
+                { 100, 'C'},
+                { 500, 'D'},
+                { 1000, 'M'}
+            };
+
+            char[] convertedNum = num.ToString().ToCharArray();
+            
+            for(int i = 0; i<convertedNum.Length; i++){
+                string thing = convertedNum[i].ToString();
+                string zeros = AddZeros(convertedNum.Length - 1 - i);
+                string actualNumberString = thing + zeros;
+                int actualNumber = Int32.Parse(actualNumberString);
+
+                while(actualNumber > 0){
+                    int nearest = 0;
+                    foreach(var numeral in numerals){
+                        if(numeral.Key<= actualNumber){
+                            nearest = numeral.Key;
+                        }
+                    }
+
+                    toReturn += numerals[nearest];
+                    actualNumber -= nearest;
+
+                    if(numerals.ContainsKey(actualNumber)){
+                        toReturn += numerals[actualNumber];
+                        actualNumber = 0;
+                    }
+
+                }
+            }
+
+            return toReturn;
+        }
+
+        public static string AddZeros(int AmmountOfZeros){
+            string toReturn = "";
+            for(int i = 0; i<AmmountOfZeros; i++){
+                toReturn += "0";
+            }
+
+            return toReturn;
+        }
+         
+
+
+
+
+        
+    }
+
+    
+ 
+}
--- LeetcodeProblemSolutions/obj/Debug/net7.0/LeetcodeProblemSolutions.AssemblyInfo.cs
+++ LeetcodeProblemSolutions/obj/Debug/net7.0/LeetcodeProblemSolutions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetcodeProblemSolutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29546c2ae173d5205b169d4325a6ca4c86680803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7363447232bc4fe3b0e01e94978aa7ed331676c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetcodeProblemSolutions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetcodeProblemSolutions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
